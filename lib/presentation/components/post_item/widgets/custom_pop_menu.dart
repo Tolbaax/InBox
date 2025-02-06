ff3cc98766acb1a073056536ae8b5341bc03bd45
Mohamed Tolba <tolbaax@gmail.com>
2025-02-06 18:42:01 +0200

    feat: add native splash

--- lib/presentation/components/post_item/widgets/custom_pop_menu.dart
+++ lib/presentation/components/post_item/widgets/custom_pop_menu.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -65,8 +66,8 @@ class CustomPubMenuButtonState extends State<CustomPubMenuButton> {
         if (value == delete) {
           AppDialogs.showConfirmDeletePost(context, onTap: () async {
             navigatePop(context);
-
             await cubit.deletePost(widget.post.postID);
+            await CachedNetworkImage.evictFromCache(widget.post.imageUrl);
             AppDialogs.showToast(msg: AppStrings.postDeleteSuccess);
           });
         }
